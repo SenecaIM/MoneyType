d521d17bc32452bf48d9253b9c0d8aaaf36432f5
Martin Taylor <martin.taylor@senecaim.com>
2017-06-13 09:41:22 +0100

    Price INotifiedProperryChanged

--- Money/Price.cs
+++ Money/Price.cs
@@ -1,14 +1,39 @@
-﻿namespace FinancialTypes
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FinancialTypes
 {
-    public class Price : ICurrencyInfoBased
+    public class Price : INotifyPropertyChanged, ICurrencyInfoBased
     {
         public Price(decimal value, string iso)
         {
             Value = value;
             CurrencyInfo = CurrencyInfoCollection.GetCurrencyInfo(iso);
         }
-        public decimal Value { get; set; }
 
-        public CurrencyInfo CurrencyInfo { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private decimal mvalue = 0;
+        public decimal Value { get => mvalue; set => SetField(ref mvalue, value); }
+
+        private CurrencyInfo currencyinfo;
+        public CurrencyInfo CurrencyInfo { get => currencyinfo; set => SetField(ref currencyinfo, value); }
+
+        protected bool SetField<T>(ref T field, T value,
+        [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
